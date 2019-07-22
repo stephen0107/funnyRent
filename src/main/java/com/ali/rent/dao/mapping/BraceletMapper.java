e7e12d0f61e0d2e603722ef80d5df0343809d7a2
zhanming <zhanming@zhanmingdeMacBook-Pro.local>
2019-07-22 22:06:20 +0800

    Initial commit

---
+++ src/main/java/com/ali/rent/dao/mapping/BraceletMapper.java
@@ -0,0 +1,7 @@
+package com.ali.rent.dao.mapping;
+
+import com.ali.rent.pojo.Bracelet;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface BraceletMapper extends Mapper<Bracelet> {
+}
---
+++ src/main/java/com/ali/rent/dao/mapping/StandardMapper.java
@@ -0,0 +1,7 @@
+package com.ali.rent.dao.mapping;
+
+import com.ali.rent.pojo.Standard;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface StandardMapper extends Mapper<Standard> {
+}
