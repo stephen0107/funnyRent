988725d209074bec6945e8f8c9059bb01da6dcbc
stephen0107 <308838535@qq.com>
2019-07-23 21:03:51 +0800

    update test1

---
+++ src/test/java/com/ali/dao/mappinng/HosterMapperTest.java
@@ -0,0 +1,27 @@
+package com.ali.dao.mappinng;
+
+import com.ali.rent.dao.mapping.HosterMapper;
+import com.ali.rent.pojo.Hoster;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring-context.xml",
+        "classpath:spring-context-druid.xml",
+        "classpath:spring-context-mybatis.xml"})
+public class HosterMapperTest {
+    @Resource
+    private HosterMapper hosterMapper;
+    @Test
+    public void testSelectAll(){
+        List<Hoster> hosters = hosterMapper.selectAll();
+        hosters.forEach(hoster -> {
+            System.out.println(hoster.getPhone());
+        });
+    }
+}
