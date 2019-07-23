4cb19656ea1374293109c428e2f5908b22bda29d
1257217791 <1257217791@qq.com>
2019-07-23 16:47:28 +0800

    hostermassage(房东详细信息)，hostermassage.jsp,HosterService层

--- src/main/java/com/ali/rent/dao/mapping/HosterMapper.java
+++ src/main/java/com/ali/rent/dao/mapping/HosterMapper.java
@@ -1,7 +1,12 @@
 package com.ali.rent.dao.mapping;
 
 import com.ali.rent.pojo.Hoster;
+import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
-public interface HosterMapper extends Mapper<Hoster> {
+import java.util.List;
+@Repository
+public interface HosterMapper extends Mapper<Hoster>
+{
+
 }
