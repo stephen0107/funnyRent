42d3519e27258157327ed30b126ecf53ac0f1a94
zhanming <1027090526@qq.com>
2019-07-23 09:32:56 +0800

    update entity

--- src/main/java/com/ali/rent/dao/mapping/BraceletMapper.java
+++
@@ -1,7 +0,0 @@
-package com.ali.rent.dao.mapping;
-
-import com.ali.rent.pojo.Bracelet;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface BraceletMapper extends Mapper<Bracelet> {
-}
--- src/main/java/com/ali/rent/dao/mapping/StandardMapper.java
+++
@@ -1,7 +0,0 @@
-package com.ali.rent.dao.mapping;
-
-import com.ali.rent.pojo.Standard;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface StandardMapper extends Mapper<Standard> {
-}
