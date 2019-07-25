fa556e5b4e849081039181107387b4f4c78384f8
stephen0107 <308838535@qq.com>
2019-07-25 10:46:20 +0800

    update 图片

--- src/main/java/com/ali/rent/controller/RoomController.java
+++ src/main/java/com/ali/rent/controller/RoomController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 
 
 
@@ -20,11 +20,20 @@ public class RoomController {
     @Autowired
     private RoomService roomService;
 
-
+    /**
+     * 通过ID获取前端信息
+     * @param id
+     * @param modelMap
+     * @return
+     */
     @RequestMapping("/{id}")
     public String showPic(@PathVariable("id") Integer id, ModelMap modelMap){
 
         modelMap.addAttribute("room",roomService.getRoomById(id));
         return "24_Property_Single";
+    }
+    @RequestMapping("/01.Home")
+    public String toHome(){
+        return "redirect:/home";
     }
 }
